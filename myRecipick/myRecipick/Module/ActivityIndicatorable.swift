658cbc7690640f9dbcf2ec8b9cbb03c630b2c9ca
김성민 <seongmin.kim@thingsflow.kr>
2021-05-05 15:12:55 +0900

    인디케이터 추가.

---
+++ myRecipick/myRecipick/Module/ActivityIndicatorable.swift
@@ -0,0 +1,50 @@
+//
+//  ActivityIndicatorable.swift
+//  myRecipick
+//
+//  Created by Min on 2021/05/05.
+//  Copyright © 2021 depromeet. All rights reserved.
+//
+
+import UIKit
+
+protocol ActivityIndicatorable where Self: UIViewController {
+    func startIndicatorAnimating()
+    func stopIndicatorAnimating()
+}
+
+private var activityIndicatorableAssociatedObjectKey: Void?
+
+extension ActivityIndicatorable {
+    private var indicatorView: UIActivityIndicatorView {
+        get {
+            var indicator = objc_getAssociatedObject(self, &activityIndicatorableAssociatedObjectKey) as? UIActivityIndicatorView
+            if indicator == nil {
+                indicator = UIActivityIndicatorView()
+                if #available(iOS 13.0, *) {
+                    indicator?.style = .large
+                } else {
+                    indicator?.style = .whiteLarge
+                }
+                objc_setAssociatedObject(self, &activityIndicatorableAssociatedObjectKey, indicator, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+            return indicator!
+        }
+        set {
+            objc_setAssociatedObject(self, &activityIndicatorableAssociatedObjectKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    func startIndicatorAnimating() {
+        view.addSubview(indicatorView)
+        indicatorView.centerInSuperview()
+        view.bringSubviewToFront(indicatorView)
+        indicatorView.startAnimating()
+    }
+    
+    func stopIndicatorAnimating() {
+        indicatorView.removeFromSuperview()
+    }
+}
+
+
