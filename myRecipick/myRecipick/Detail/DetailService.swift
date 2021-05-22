1259abd407ccf779abddb02f4fbec5acf2ec2d46
Hanwe Lee <mobiledev@kakao.com>
2021-05-22 15:35:01 +0900

    - API 수정사항 반영

--- myRecipick/myRecipick/Detail/DetailService.swift
+++ myRecipick/myRecipick/Detail/DetailService.swift
@@ -59,62 +59,43 @@ class DetailService: DetailServiceProtocol {
 struct CustomMenuDetailObjModel: JsonDataProtocol {
     var id: String = ""
     var userId: String = ""
-    var menuId: String = ""
     var name: String = ""
     var optionGroups: [CustomMenuDetailOptionGroupObjModel] = []
     var createdDate: String = ""
     var updatedDate: String = ""
-    var isShow: Bool = false
     
     enum CodingKeys: String, CodingKey {
         case id
         case userId
-        case menuId
         case name
         case optionGroups
         case createdDate
         case updatedDate
-        case isShow = "show"
     }
 }
 
 struct CustomMenuDetailOptionGroupObjModel: JsonDataProtocol {
     var id: String = ""
-    var type: String = ""
     var name: String = ""
     var imageUrl: String?
     var options: [CustomMenuDetailOptionGroupOptionsObjModel] = []
-    var policy: CustomMenuDetailOptionGroupPolicyObjModel = CustomMenuDetailOptionGroupPolicyObjModel()
-    var createdDate: String?
-    var updatedDate: String?
     
     enum CodingKeys: String, CodingKey {
         case id
-        case type
         case name
         case imageUrl = "image"
         case options
-        case policy
-        case createdDate
-        case updatedDate
     }
 }
 
 struct CustomMenuDetailOptionGroupOptionsObjModel: JsonDataProtocol {
-    var type: String = ""
     var name: String = ""
     var imageUrl: String?
     var category: String?
     
     enum CodingKeys: String, CodingKey {
-        case type
         case name
         case imageUrl = "image"
         case category
     }
-}
-
-struct CustomMenuDetailOptionGroupPolicyObjModel: JsonDataProtocol {
-    var min: Int = -1
-    var max: Int = -1
 }
