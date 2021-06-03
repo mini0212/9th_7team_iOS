c8ca25f3e136c4b4409e46f724a05c96515795c1
Hanwe Lee <mobiledev@kakao.com>
2021-06-03 21:26:30 +0900

    - 브랜드 뷰 디렉토리 위치 변경  - 마이페이지에서 햄버거메뉴 보이도록 수정

--- myRecipick/myRecipick/Tabs/YourPage/YourPageCoordinator.swift
+++ myRecipick/myRecipick/Tabs/YourPage/YourPageCoordinator.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SideMenu
 
 protocol YourPageCoordinatorDelegate: AnyObject {
     func editBtnClicked()
@@ -15,6 +16,7 @@ protocol YourPageCoordinatorDelegate: AnyObject {
 class YourPageCoordinator: MainTabCoordinatorProtocol {
     
     enum Route {
+        case requestBrand
         case detail(DetailService.DetailServiceInfoModel)
     }
     
@@ -27,9 +29,17 @@ class YourPageCoordinator: MainTabCoordinatorProtocol {
     
     weak var delegate: YourPageCoordinatorDelegate?
     
+    let sideMenuWidth: CGFloat = 232.0
+    
+    let dimmedView: UIView = UIView()
+    var isDimmed: Bool = false
+    
     // MARK: lifeCycle
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
+        self.dimmedView.frame = CGRect(x: 0, y: 0, width: self.navigationController?.view.frame.width ?? 0, height: self.navigationController?.view.frame.height ?? 0)
+        self.dimmedView.backgroundColor = UIColor(asset: Colors.dimmed)
+        self.dimmedView.alpha = 0
     }
     
     deinit {
@@ -58,6 +68,8 @@ class YourPageCoordinator: MainTabCoordinatorProtocol {
         
         self.navigationController?.navigationBar.topItem?.rightBarButtonItems?.removeAll()
         self.navigationController?.navigationBar.topItem?.rightBarButtonItem = editBtn
+        let barButtonItem = UIBarButtonItem(image: Images.uilBars.image.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(showBrandSelectView))
+        self.navigationController?.navigationBar.topItem?.leftBarButtonItem = barButtonItem
     }
     
     func removeRightBarButtonItems() {
@@ -79,6 +91,8 @@ class YourPageCoordinator: MainTabCoordinatorProtocol {
     
     func present(route: Route, animated: Bool, presentStyle: UIModalPresentationStyle = .pageSheet, completion: (() -> Void)?) {
         switch route {
+        case .requestBrand:
+            break
         case .detail(let data):
             let navigationController: UINavigationController = UINavigationController()
             let vc = DetailViewController.makeViewController(coordinator: DetailViewCoordinator(navigationController: navigationController), viewModel: DetailViewModel(service: DetailService(data: data)))
@@ -88,9 +102,55 @@ class YourPageCoordinator: MainTabCoordinatorProtocol {
         }
     }
     
+    func push(route: Route, animated: Bool) {
+        switch route {
+        case .requestBrand:
+            let vc: RequestBrandViewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(identifier: RequestBrandViewController.identifier)
+            self.navigationController?.pushViewController(vc, animated: animated)
+        case .detail:
+            break
+        }
+    }
+    
     // MARK: action
     
     @objc func editAction(_ sender: UIButton) {
         self.delegate?.editBtnClicked()
     }
+    
+    @objc func showBrandSelectView(sender: UIBarButtonItem) {
+        guard let brandSelectViewController: BrandSelectViewController = BrandSelectViewController.makeViewController(viewModel: BrandSelectViewModel(service: BrandSelectService())) else { return }
+        brandSelectViewController.delegate = self
+        let menu = SideMenuNavigationController(rootViewController: brandSelectViewController)
+        menu.leftSide = true
+        menu.menuWidth = self.sideMenuWidth
+        menu.presentationStyle = .menuSlideIn
+        menu.sideMenuDelegate = self
+        
+        self.navigationController?.present(menu, animated: true, completion: nil)
+    }
 }
+
+extension YourPageCoordinator: SideMenuNavigationControllerDelegate {
+    func sideMenuWillAppear(menu: SideMenuNavigationController, animated: Bool) {
+        if !self.isDimmed {
+            self.isDimmed = true
+            self.navigationController?.view.addSubview(self.dimmedView)
+            self.dimmedView.fadeIn(duration: 0.1, completeHandler: nil)
+        }
+    }
+    
+    func sideMenuDidDisappear(menu: SideMenuNavigationController, animated: Bool) {
+        self.dimmedView.fadeOut(duration: 0.1, completeHandler: { [weak self] in
+            self?.dimmedView.removeFromSuperview()
+            self?.isDimmed = false
+        })
+    }
+}
+
+extension YourPageCoordinator: BrandSelectViewControllerDelegate {
+    func pushRequestBrandViewController() {
+        push(route: .requestBrand, animated: true)
+    }
+}
+
